e999f242abf71f3b7b964c5c676ef5d04fbe2c81
DarshanSM26 <DARSHAN.20211COM0040@presidencyuniversity.in>
2025-08-13 10:13:04 +0530

    product added

---
+++ src/main/java/com/Group3tatastrive/VetPawtner/VetPawtnerApplication.java
@@ -0,0 +1,83 @@
+package com.Group3tatastrive.VetPawtner;
+
+import com.Group3tatastrive.VetPawtner.Entity.*;
+import com.Group3tatastrive.VetPawtner.Repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
+
+
+@SpringBootApplication
+public class VetPawtnerApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetPawtnerApplication.class, args);
+	}
+
+	@Autowired
+	UserRepository userRepository;
+	@Autowired
+	OrderRepository orderRepository;
+	@Autowired
+	DashboardRepository dashboardRepository;
+	@Autowired
+	PetRepository petRepository;
+
+	@Autowired
+	ProductRepository productRepository;
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		String email = "darshi@example.com";
+		User user; // declare before if
+		if (!userRepository.existsByEmail(email)) {
+			user = new User();
+			user.setRole(User.Role.user);
+			userRepository.save(user);
+		} else {
+			user = userRepository.findByEmail(email).orElseThrow(null);
+		}
+
+		Order order  = new Order();
+		order.setOrderId(order.getOrderId());
+		order.setUser(order.getUser());
+		order.setProduct(order.getProduct());
+		order.setQuantity(35);
+		order.setTotalPrice(1200.0);
+		order.setOrderDate(LocalDate.of(2025,8,4));
+		order.setStatus(order.getStatus());
+
+
+
+
+		Dashboard dashboard = new Dashboard();
+		dashboard.setUser(user);
+		dashboard.setTotalPets(1);
+		dashboard.setTotalOrders(0);
+		dashboardRepository.save(dashboard);
+
+		Pet pet = new Pet();
+		pet.setUser(user);
+		pet.setName("Tommy");
+		pet.setBreed("Labrador");
+		pet.setGender(Pet.Gender.male);
+		pet.setDob(LocalDate.of(2025, 8, 10));
+		pet.setVaccinationDetails("Rabies, Distemper");
+		petRepository.save(pet);
+
+		Product product = new Product();
+		/*product.setName("Darshan");
+		product.setDescription("Toy bone item");
+		product.setPrice(200);
+		product.setStock(2);
+		product.setCategory("Toy");
+		product.setImage_url("https:localhost:8080");
+		productRepository.save(product);*/
+
+	}
+
+}
